b7996e734bb7507b54bd2f052213e06405917dff
Mai <ivasiutamagdalena@gmail.com>
2026-07-15 08:56:33 -0300

    Se crea el archivo main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("===================================")
+    print(" Calculadora de Álgebra Lineal")
+    print("===================================")
+    print("Proyecto iniciado correctamente.")
+
+
+if __name__ == "__main__":
+    main()
