7f1a912d17280ec2a4c3c0bbf8b0e0fdcdded173
Mai <ivasiutamagdalena@gmail.com>
2026-07-15 10:59:50 -0300

    Se creo una lista de temas para que la interfaz se construya automáticamente

--- gui/app.py
+++ gui/app.py
@@ -13,20 +13,84 @@ class App(ctk.CTk):
         self.minsize(800, 500) # evita que el usuario reduzca demasiado la ventana
 
         # Configuración del tema y color de la ventana
+
         ctk.set_appearance_mode("System") # establece el modo de apariencia del sistema operativo
         ctk.set_default_color_theme("blue") # establece el tema de colores por defecto
 
         # Configuración de la cuadrícula principal
+
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
         # === Panel lateral ===
-        self.sidebar = ctk.CTkFrame(self, width=250, corner_radius=0)
-        self.sidebar.grid(row=0, column=0, sticky="ns")
+        
+        self.sidebar = ctk.CTkFrame(
+            self, 
+            width=250, 
+            corner_radius=0
+        )
+
+        self.sidebar.grid(
+            row=0, 
+            column=0, 
+            sticky="ns"
+        )
 
         # === Área principal ===
+
         self.main_frame = ctk.CTkFrame(self)
-        self.main_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
+        self.main_frame.grid(
+            row=0, 
+            column=1, 
+            sticky="nsew", 
+            padx=20, 
+            pady=20
+        )
 
         self.main_frame.grid_columnconfigure(0, weight=1)
         self.main_frame.grid_rowconfigure(0, weight=1)
+
+        # Título de la calculadora
+
+        self.title_label = ctk.CTkLabel(
+            self.sidebar,
+            text="ÁLGEBRA\nLINEAL",
+            font=("Segoe UI", 22, "bold")
+        )
+
+        self.title_label.pack(
+            pady=(25,35)
+        )
+
+        # Botones
+        self.modules = [
+            "Sistemas de ecuaciones",
+            "Matrices",
+            "Determinantes",
+            "Vectores",
+            "Grafos",
+            "Rectas y planos",
+            "Espacios vectoriales",
+            "Aplicaciones lineales"
+        ]
+
+        for module in self.modules:
+
+            button = ctk.CTkButton(
+                self.sidebar,
+                text=module,
+                width=200,
+                height=40
+            )
+
+        # Mensaje principal
+        self.welcome = ctk.CTkLabel(
+            self.main_frame,
+            text=(
+                "Bienvenido a la Calculadora de Álgebra Lineal.\n\n"
+                "Seleccione un módulo en el panel lateral para comenzar."
+            ),
+            font=("Segoe UI", 22)
+        )
+
+        self.welcome.pack(expand=True)
