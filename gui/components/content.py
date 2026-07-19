acf19ef156f4b5570e95eb4090e0d23f7998c66f
Mai <ivasiutamagdalena@gmail.com>
2026-07-19 07:35:05 -0300

    Se modifica el código de content.py

--- gui/components/content.py
+++ gui/components/content.py
@@ -2,6 +2,8 @@ import customtkinter as ctk
 
 from gui.pages.home_page import HomePage
 
+from gui.pages.matrices_page import MatriccesPage
+
 class ContentFrame(ctk.CTkFrame):
 
     def __init__(self, master):
@@ -18,3 +20,21 @@ class ContentFrame(ctk.CTkFrame):
             column=0,
             sticky="nsew"
         )
+
+    def show_page(self, page_name):
+            
+            # Elimina la página actual
+            self.current_page.destroy()
+
+            # Selecciona la nueva página
+            if page_name == "Matrices":
+                self.current_page = MatricesPage(self)
+            else:
+                self.current_page = HomePage(self)
+
+            # Muestra la página
+            self.current_page.grid(
+                 row=0,
+                 column=0,
+                 sticky="nsew"
+            )
